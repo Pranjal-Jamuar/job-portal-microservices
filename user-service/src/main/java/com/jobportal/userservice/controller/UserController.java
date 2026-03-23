1a65cdc47d06411dd6b466249a6cd79cb2ce648e
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2026-03-23 15:12:25 +0530

    Refactor login API to return UserResponse instead of entity

--- user-service/src/main/java/com/jobportal/userservice/controller/UserController.java
+++ user-service/src/main/java/com/jobportal/userservice/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.jobportal.userservice.controller;
 
+import com.jobportal.userservice.dto.UserResponse;
 import com.jobportal.userservice.entity.User;
 import com.jobportal.userservice.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.jobportal.userservice.dto.LoginRequest;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,5 +22,19 @@ public class UserController {
     public ResponseEntity<User> register(@RequestBody User user) {
         User savedUser = userService.registerUser(user);
         return ResponseEntity.ok(savedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody LoginRequest request) {
+
+        User user = userService.login(request.getEmail(), request.getPassword());
+
+        UserResponse response = new UserResponse();
+        response.setUserId(user.getUserId());
+        response.setName(user.getName());
+        response.setEmail(user.getEmail());
+        response.setRole(String.valueOf(user.getRole()));
+
+        return ResponseEntity.ok(user);
     }
 }
