09092f809ce914e89f2048aa83935decac1e83ca
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2026-03-23 14:30:46 +0530

    Added User entity and Role enum

---
+++ user-service/src/main/java/com/jobportal/userservice/entity/User.java
@@ -0,0 +1,27 @@
+package com.jobportal.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
