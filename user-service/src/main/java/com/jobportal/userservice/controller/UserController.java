f500103dacbb13e128c17e1c586491da059ec99c
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2026-03-23 14:31:49 +0530

    Exposed user registration API endpoint

---
+++ user-service/src/main/java/com/jobportal/userservice/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.jobportal.userservice.controller;
+
+import com.jobportal.userservice.entity.User;
+import com.jobportal.userservice.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
