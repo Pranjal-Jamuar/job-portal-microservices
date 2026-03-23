bb53d4df4b89e41eb22fc59688b10c9ab19570bb
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2026-03-23 15:11:04 +0530

    Add email uniqueness validation and integrate password hashing in registration flow

--- user-service/src/main/java/com/jobportal/userservice/service/UserService.java
+++ user-service/src/main/java/com/jobportal/userservice/service/UserService.java
@@ -2,6 +2,7 @@ package com.jobportal.userservice.service;
 
 import com.jobportal.userservice.entity.User;
 import com.jobportal.userservice.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -10,17 +11,34 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(User user) {
-
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already registered");
+        }
         // Generate unique userId
         long count = userRepository.count() + 1;
         user.setUserId("U" + (100 + count));
 
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public User login(String email, String password) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return user;
     }
 }
