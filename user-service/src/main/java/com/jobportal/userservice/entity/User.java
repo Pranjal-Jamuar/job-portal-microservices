6bac4693ab002b13b3196af32d90af09f73808b6
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2026-03-27 11:52:14 +0530

    feat: update user entity with authentication fields

--- user-service/src/main/java/com/jobportal/userservice/entity/User.java
+++ user-service/src/main/java/com/jobportal/userservice/entity/User.java
@@ -3,12 +3,14 @@ package com.jobportal.userservice.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
 
     @Id
     private String userId;
@@ -22,6 +24,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
 }
